3b5ffa69fb8a54710e090520c54d7ae1d7110bbe
Jarred Hone <tailcracker@gmail.com>
2015-04-02 02:47:43 +1300

    Made Person class
    (Instances)

---
+++ src/main/Person.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * This class represents a single instance on the dataset, in this case it is a person
+ * @author Jarred Hone
+ *
+ */
+public class Person {
+
+	String category;
+	private List<Boolean> values = new ArrayList<Boolean>();
+
+
+
+	public Person(String category, List<Boolean> values){
+		this.category =  category;
+		this.values = values;
+
+	}
+
+
+	/**
+	 * Returns the list of true/false values for outside access
+	 * @return
+	 */
+	public List<Boolean> getValues(){
+		return this.values;
+	}
+
+	/**
+	 * Getter to return private field category
+	 * @return String category
+	 */
+	public String getCategory(){
+		return category;
+	}
+
+
+}
